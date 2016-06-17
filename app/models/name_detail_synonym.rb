1252bd2eedc72da55b5302be7025eb11877fe95d
Greg Clarke <gregbclarke@gmail.com>
2016-06-17 14:47:47 +1000

    Label a synonym of type misapplied as a misapplication.

--- app/models/name_detail_synonym.rb
+++ app/models/name_detail_synonym.rb
@@ -1,4 +1,13 @@
 class NameDetailSynonym < ActiveRecord::Base
   self.table_name = "name_detail_synonyms_vw"
   belongs_to :name_detail, foreign_key: :cited_by_id
+
+  def label
+    case instance_type_name
+    when 'misapplied'
+      'misapplication'
+    else
+      instance_type_name
+    end
+  end
 end
