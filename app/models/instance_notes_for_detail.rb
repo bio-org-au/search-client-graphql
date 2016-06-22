8cf3dc66fc04574413d9510c06a27e3161132c6a
Greg Clarke <gregbclarke@gmail.com>
2016-06-22 16:39:59 +1000

    Show type, lectotype, neotype notes above synonyms.

--- app/models/instance_notes_for_detail.rb
+++ app/models/instance_notes_for_detail.rb
@@ -2,6 +2,8 @@ class InstanceNotesForDetail < ActiveRecord::Base
   self.table_name = "instance_note"
   self.primary_key = "id"
   default_scope { where("instance_note_key_id in (select id from instance_note_key where not deprecated and name not like 'APC%' and name not like 'EPBC%') ") }
+  scope :only_type_notes, -> { where("instance_note_key_id in (select id from instance_note_key where name in ('Lectotype', 'Neotype','Type') ) ") }
+  scope :without_type_notes, -> { where("instance_note_key_id not in (select id from instance_note_key where name in ('Lectotype', 'Neotype','Type') ) ") }
 
   belongs_to :instance
   belongs_to :instance_note_key
