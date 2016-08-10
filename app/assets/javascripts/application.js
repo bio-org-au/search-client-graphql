b0302e8c56b6f9a89a3692a8515895a61c981ff1
Greg Clarke <gregbclarke@gmail.com>
2016-08-10 17:39:38 +1000

    Noted turbolinks removed.  Did that in previous commit to get around JS problems.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// Turned off turbolinks.
+//
 //= require jquery
 //= require tether
 //= require bootstrap
