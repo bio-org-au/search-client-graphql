708b051e4f6b4ee997af9fd10b082541b8b079a7
Greg Clarke <gregbclarke@gmail.com>
2016-09-01 14:58:07 +1000

    Get shard from environment variable; look for shard-specific config file.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,23 +45,23 @@ end
 
 begin
   raise "no_shard_set" if (ENV["SHARD"]).nil?
-  puts %(Configuring shard: #{ENV["SHARD"]})
-rescue => e
-  puts "="*100
+  puts %(Configuring shard: #{ENV['SHARD']})
+rescue
+  puts "=" * 100
   puts "Expected the SHARD environmental variable to be set."
   puts "Application start up will now fail."
-  puts "="*100
+  puts "=" * 100
   raise
 end
 
 begin
-  file_path = "#{ENV['HOME']}/.nsl/development/#{ENV["SHARD"]}-search-config.rb"
+  file_path = "#{ENV['HOME']}/.nsl/development/#{ENV['SHARD']}-search-config.rb"
   puts "Loading config from: #{file_path}"
   load file_path
 rescue LoadError
-  puts "="*100
+  puts "=" * 100
   puts "Unable to find the config file: #{file_path}"
   puts "Application start up will now fail."
-  puts "="*100
+  puts "=" * 100
   raise
 end
