aff0c60a49d73cfdc5868ce9b541799733810924
Greg Clarke <gregbclarke@gmail.com>
2016-10-19 16:28:41 +1100

    Another change to display name status after any non-legit name in synonymy.

--- app/models/name_detail_synonym.rb
+++ app/models/name_detail_synonym.rb
@@ -5,6 +5,7 @@ class NameDetailSynonym < ActiveRecord::Base
   self.table_name = "name_detail_synonyms_vw"
   belongs_to :name_detail, foreign_key: :cited_by_id
   belongs_to :cites, class_name: "Instance", foreign_key: :cites_id
+  belongs_to :name
 
   def label
     instance_type_name.sub(/misapplied/, "misapplication")
