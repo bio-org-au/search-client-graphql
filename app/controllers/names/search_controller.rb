fc8db1fb61c7ae64fb8ca7317819f15880a85dff
Greg Clarke <gregbclarke@gmail.com>
2017-09-15 15:40:25 +1000

    Make search index respond with plain text string

--- app/controllers/names/search_controller.rb
+++ app/controllers/names/search_controller.rb
@@ -3,26 +3,6 @@
 # Controller
 class Names::SearchController < ApplicationController
   def index
-    search if params["q"].present?
-    respond_to do |format|
-      format.html
-      format.json { render json: @search }
-      format.csv
-    end
-  end
-
-  private
-
-  def search
-    @search = case params[:search_type]
-              when /scientific.and.cultivar\z/i
-                Names::Search::ScientificAndCultivar.new(params)
-              when /cultivar\z/i
-                Names::Search::Cultivar.new(params)
-              when /common\z/i
-                Names::Search::Common.new(params)
-              else
-                Names::Search::Scientific.new(params)
-              end
+    render plain: 'index'
   end
 end
