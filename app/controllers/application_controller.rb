5f7143be15509272c00147a903abeb488b744a1e
Greg Clarke <gregbclarke@gmail.com>
2017-10-23 16:09:03 +1100

    Show name and taxonomy labels retrieved from the server.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :start_timer
+  before_action :start_timer, :settings
   private
   def start_timer
     @start_time = Time.now
+  end
+
+  def settings
+    @setting = Setting.new
+    @name_label = @setting.name_label
+    @taxonomy_label = @setting.taxonomy_label
   end
 end
---
+++ app/models/setting.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Quacks like a rails model
+class Setting
+  DATA_SERVER = Rails.configuration.data_server
+
+  def name_label
+    @name_label ||= ask_for_setting("name label")
+  end
+
+  def tree_label
+    taxonomy_label
+  end
+
+  def taxonomy_label
+    @taxonomy_label ||= ask_for_setting("tree label")
+  end
+
+  private
+
+  def ask_for_setting(setting)
+    options = {
+                body: {
+                  query: %Q({setting(search_term: "#{setting}") })
+                      }
+              }
+    Rails.logger.debug(options.inspect)
+    json = HTTParty.post("#{DATA_SERVER}/v1",options)
+    @search = JSON.parse(json.to_s, object_class: OpenStruct)
+    @search.data.setting
+  end
+end
+
