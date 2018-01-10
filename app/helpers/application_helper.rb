f416f958f77499cb56e1d7559267ef85958a598c
Greg Clarke <gregbclarke@gmail.com>
2018-01-10 14:41:25 +1100

    Make some advanced search checkboxes checked by default.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,58 @@ module ApplicationHelper
   def rank_options
     [1,2,3,4]
   end
+
+  def scientific_autonym_field_value
+    if params['search'] == 'Search' || params['count'] == 'Count'
+      san = params['scientific_autonym_name']
+    else
+      san = '1'
+    end
+    san
+  end
+
+  def scientific_hybrid_name_field_value
+    if params['search'] == 'Search' || params['count'] == 'Count'
+      san = params['scientific_hybrid_name']
+    else
+      san = '1'
+    end
+    san
+  end
+
+  def scientific_name_field_value
+    if params['search'] == 'Search' || params['count'] == 'Count'
+      san = params['scientific_name']
+    else
+      san = '1'
+    end
+    san
+  end
+
+  def type_note_key_type_field_value
+    if params['search'] == 'Search' || params['count'] == 'Count'
+      san = params['type_note_key_type']
+    else
+      san = '1'
+    end
+    san
+  end
+
+  def type_note_key_lectotype_field_value
+    if params['search'] == 'Search' || params['count'] == 'Count'
+      san = params['type_note_key_lectotype']
+    else
+      san = '1'
+    end
+    san
+  end
+
+  def type_note_key_neotype_field_value
+    if params['search'] == 'Search' || params['count'] == 'Count'
+      san = params['type_note_key_neotype']
+    else
+      san = '1'
+    end
+    san
+  end
 end
