2e4aa40bd4b6e474ed93f464f1b671805c17bc5b
Greg Clarke <gregbclarke@gmail.com>
2018-03-07 10:40:52 +1100

    Name check limit and stats.

--- app/controllers/name_check_controller.rb
+++ app/controllers/name_check_controller.rb
@@ -4,16 +4,12 @@
 class NameCheckController < ApplicationController
   def index
     @client_request = Index::ClientRequest.new(search_params)
-    if @client_request.any_type_of_search?
-      Rails.logger.debug('there is a search')
-      @wrapped_search_results = Results.new(@client_request.search_results)
-      Rails.logger.debug("@wrapped_search_results class: #{@wrapped_search_results.class}")
-    end
+    @wrapped_search_results = Results.new(@client_request.search_results)
   end
 
   private
 
   def search_params
-    params.permit(:utf8, :q, :controller, :action, :list_or_tabular)
+    params.permit(:utf8, :q, :controller, :action, :list_or_tabular, :limit)
   end
 end
