969ef8233950cf58e127c4d7d9c088bb9c3c313e
Greg Clarke <gregbclarke@gmail.com>
2018-04-12 10:22:23 +1000

    Comment out notifications in the initializer.

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -2,6 +2,13 @@
 
 # frozen_string_literal: true
 
-require 'nunes'
-statsd = Statsd.new('127.0.0.1', 8125)
-Nunes.subscribe(statsd)
+# require 'nunes'
+# statsd = Statsd.new('127.0.0.1', 8125)
+# Nunes.subscribe(statsd)
+
+
+# ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |name, start, finish, id, payload|
+  # Rails.logger.debug(["notification:", 'name:', name, 'start:', start, 'finish:', finish, 'id:', id, 'payload:', payload].join(" "))
+# end
+
+
