607bcb955f9fce02fd1a3e3a4bfbb90672461482
Greg Clarke <gregbclarke@gmail.com>
2018-08-28 15:47:56 +1000

    Show environment query results

---
+++ app/models/runtime_environment.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# Get Runtime Environment info from the server.
+class RuntimeEnvironment
+  DATA_SERVER = Rails.configuration.data_server
+
+  def results
+    json = HTTParty.post("#{DATA_SERVER}/v1",
+                         body: body,
+                         timeout: 10)
+    a = []
+    JSON.parse(json.to_s, object_class: Hash)['data']['runtime_environment']
+  end
+
+  def database
+    results['database']
+  end
+
+  def rails_env
+    results['rails_env']
+  end
+
+  private
+
+  def body
+    { query: graphql_query_string }
+  end
+
+  def graphql_query_string
+    <<~HEREDOC
+        {runtime_environment()
+          {database,
+           ruby_version,
+           ruby_platform,
+           rails_version,
+           rails_env}
+        }
+    HEREDOC
+  end
+end
