ebefe8eef156c49d354c407be33cd1d09e625e53
Greg Clarke <gregbclarke@gmail.com>
2018-09-25 15:27:23 +1000

    Remove database components from test helper; run curl test.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,17 +5,17 @@ SimpleCov.start
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+# require 'rails/test_help'
 # require 'minitest/autorun'
-require 'mocha'
-require 'mocha/setup'
-require 'mocha/mini_test'
+# require 'mocha'
+# require 'mocha/setup'
+# require 'mocha/mini_test'
 
 # Set up for all tests.
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
   # order.
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
